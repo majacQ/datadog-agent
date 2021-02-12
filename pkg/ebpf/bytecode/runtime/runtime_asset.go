82c8dd1fbcbf5ce8efc4400231731a056f0a7c63
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2021-02-11 22:29:07 -0500

    Temp changes for testing

--- pkg/ebpf/bytecode/runtime/runtime_asset.go
+++ pkg/ebpf/bytecode/runtime/runtime_asset.go
@@ -15,6 +15,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/ebpf"
 	"github.com/DataDog/datadog-agent/pkg/ebpf/compiler"
 	"github.com/DataDog/datadog-agent/pkg/util/kernel"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 var (
@@ -129,7 +130,7 @@ func (a *RuntimeAsset) Compile(config *ebpf.Config, cflags []string) (CompiledOu
 	// this ensures we re-compile when either of the input changes
 	baseName := strings.TrimSuffix(a.filename, filepath.Ext(a.filename))
 	outputFile := filepath.Join(config.RuntimeCompilerOutputDir, fmt.Sprintf("%s-%d-%s-%s.o", baseName, kv, hash, flagHash))
-	if _, err := os.Stat(outputFile); err != nil {
+	if _, err = os.Stat(outputFile); err != nil {
 		if !os.IsNotExist(err) {
 			a.compilationResult = outputFileErr
 			return nil, fmt.Errorf("error stat-ing output file %s: %w", outputFile, err)
@@ -145,6 +146,8 @@ func (a *RuntimeAsset) Compile(config *ebpf.Config, cflags []string) (CompiledOu
 			a.compilationResult = compilationErr
 			return nil, fmt.Errorf("failed to compile runtime version of %s: %s", a.filename, err)
 		}
+	} else {
+		log.Infof("**** Output file already exists: %s", outputFile)
 	}
 
 	out, err := os.Open(outputFile)
